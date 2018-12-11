304b3790dbc397c5c33dfec4167c40da7fd37335
Ben Wiley <therealbenwiley@gmail.com>
2018-12-10 23:47:41 -0500

    Replace app content with cassette media player.

---
+++ src/playlist.js
@@ -0,0 +1,155 @@
+export default [
+  {
+    url:
+      'https://ia801406.us.archive.org/9/items/BillyWilliams_288/ATaleOfParis1910_64kb.mp3',
+    title: 'A Tale of Paris',
+    artist: 'Billy Williams',
+    album: 'Cassette',
+    artwork: [
+      {
+        src:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/When_Father_Papered_The_Parlour.jpg/800px-When_Father_Papered_The_Parlour.jpg'
+      }
+    ],
+    meta: {
+      archiveDotOrgUrl:
+        'https://archive.org/details/BillyWilliams_288/ATaleOfParis1910.mp3'
+    }
+  },
+  {
+    url:
+      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+    title: 'Big Buck Bunny',
+    artist: 'Peach Open Movie Project'
+  },
+  {
+    url:
+      'https://ia800800.us.archive.org/24/items/78_blue-skies_benny-goodman-and-his-orchestra-art-lund-berlin_gbia0019543b/Blue%20Skies%20-%20Benny%20Goodman%20and%20his%20Orchestra.mp3',
+    title: 'Blue Skies',
+    artist: 'Benny Goodman and his Orchestra',
+    album: 'Cassette',
+    artwork: [
+      {
+        src:
+          'https://img.discogs.com/CicybooovCx5Z1ujeC75HppHWR0=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-3463956-1487530941-4769.jpeg.jpg'
+      }
+    ],
+    meta: {
+      archiveDotOrgUrl:
+        'https://archive.org/details/78_blue-skies_benny-goodman-and-his-orchestra-art-lund-berlin_gbia0019543b'
+    }
+  },
+  {
+    url:
+      'https://ia600302.us.archive.org/28/items/sourire1906/sourire1906_64kb.mp3',
+    title: "Sourire d'Avril",
+    artist: 'Columbia Orchestra',
+    album: 'Cassette',
+    artwork: [
+      {
+        src:
+          'https://publicdomainreview.org/wp-content/uploads/2015/07/965_001-540x362.jpg'
+      }
+    ],
+    meta: {
+      archiveDotOrgUrl: 'https://archive.org/details/sourire1906'
+    }
+  },
+  {
+    url:
+      'https://ia800200.us.archive.org/2/items/AdaJoneswithVictorLightOperaCompany/AdaJoneswithVictorLightOperaCompany-TheYamaYamaMan_2.mp3',
+    title: 'Yama Yama Man',
+    artist: 'Ada Jones',
+    album: 'Cassette',
+    artwork: [
+      {
+        src: 'https://travsd.files.wordpress.com/2011/09/q3jmb58n11d55dn.jpg'
+      }
+    ],
+    meta: {
+      archiveDotOrgUrl:
+        'https://archive.org/details/AdaJoneswithVictorLightOperaCompany/AdaJoneswithVictorLightOperaCompany-TheYamaYamaMan_2.mp3'
+    }
+  },
+  {
+    url:
+      'https://ia801400.us.archive.org/2/items/rhapblue11924/rhapblue11924_64kb.mp3',
+    title: 'Rhapsody in Blue pt. 1',
+    artist: 'Paul Whiteman and George Gershwin',
+    album: 'Cassette',
+    artwork: [
+      {
+        src:
+          'https://upload.wikimedia.org/wikipedia/commons/d/d5/George_gershwin.jpg'
+      }
+    ],
+    meta: {
+      archiveDotOrgUrl: 'https://archive.org/details/rhapblue11924'
+    }
+  },
+  {
+    url:
+      'https://ia801406.us.archive.org/27/items/EDIS-SRP-0198-15/EDIS-SRP-0198-15.mp3',
+    title: 'Barbara Allen',
+    artist: 'Frank Luther',
+    album: 'Cassette',
+    artwork: [
+      {
+        src:
+          'https://upload.wikimedia.org/wikipedia/commons/d/dc/Forget_Me_Not_Songster_-_Barbara_Allen_p.1.jpg'
+      }
+    ],
+    meta: {
+      archiveDotOrgUrl: 'https://archive.org/details/EDIS-SRP-0198-15'
+    }
+  },
+  {
+    url:
+      'https://ia800309.us.archive.org/14/items/rhapblue21924/rhapblue21924_64kb.mp3',
+    title: 'Rhapsody in Blue pt. 2',
+    artist: 'Paul Whiteman and George Gershwin',
+    album: 'Cassette',
+    artwork: [
+      {
+        src:
+          'https://upload.wikimedia.org/wikipedia/commons/d/d5/George_gershwin.jpg'
+      }
+    ],
+    meta: {
+      archiveDotOrgUrl: 'https://archive.org/details/rhapblue21924'
+    }
+  },
+  {
+    url:
+      'https://ia601409.us.archive.org/11/items/MidsommerDansen--scandinavian/MidsommerDansen--scandinavian.mp3',
+    title: 'Midsommer Schottische',
+    album: 'Cassette',
+    artwork: [
+      {
+        src:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Fem_midsommarfirande_v%C3%A4garbetare_i_atelj%C3%A9n._V%C3%A4gbygget_gick_mellan_Lima_och_R%C3%B6rb%C3%A4cksn%C3%A4s%2C_1901-1905_-_Nordiska_Museet_-_NMA.0052748.jpg/487px-Fem_midsommarfirande_v%C3%A4garbetare_i_atelj%C3%A9n._V%C3%A4gbygget_gick_mellan_Lima_och_R%C3%B6rb%C3%A4cksn%C3%A4s%2C_1901-1905_-_Nordiska_Museet_-_NMA.0052748.jpg'
+      }
+    ],
+    meta: {
+      archiveDotOrgUrl:
+        'https://archive.org/details/MidsommerDansen--scandinavian'
+    }
+  },
+  {
+    url:
+      'https://ia800907.us.archive.org/15/items/RACHMANINOFFRhapsodyOnAThemeByPaganini-Rachmaninoff-NEWTRANSFER/Rachmaninoff-RhapsodyOnAThemeByPaganiniOp.43.mp3',
+    title: 'Rhapsody on a Theme of Paganini',
+    artist: 'Sergei Rachmaninoff',
+    album: 'Cassette',
+    artwork: [
+      {
+        src:
+          'https://upload.wikimedia.org/wikipedia/commons/b/be/Sergei_Rachmaninoff_cph.3a40575.jpg'
+      }
+    ],
+    meta: {
+      archiveDotOrgUrl:
+        'https://archive.org/details/RACHMANINOFFRhapsodyOnAThemeByPaganini-Rachmaninoff-NEWTRANSFER'
+    }
+  }
+];
