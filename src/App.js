ef062d20740e4a0a7b047e39070829eaff464505
Ben Wiley <therealbenwiley@gmail.com>
2018-12-10 23:47:41 -0500

    Replace app content with cassette media player.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { MediaPlayer } from '@cassette/player';
+import '@cassette/player/dist/css/cassette-player.css';
+
+import playlist from './playlist';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <MediaPlayer playlist={playlist} />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
